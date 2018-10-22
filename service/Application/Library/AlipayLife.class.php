40536fb1176b7e6010423e9f169dcaf93bd68c7a
devil_gong <devil_gong@wochacha.com>
2018-10-22 17:49:00 +0800

    shh

--- service/Application/Library/AlipayLife.class.php
+++ service/Application/Library/AlipayLife.class.php
@@ -121,7 +121,6 @@ class AlipayLife
      */
     private function MyRsaSign($prestr)
     {
-        echo $prestr;die;
         $res = "-----BEGIN PRIVATE KEY-----\n";
         $res .= wordwrap($this->life_data['rsa_private'], 64, "\n", true);
         $res .= "\n-----END PRIVATE KEY-----";
