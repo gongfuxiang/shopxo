e78d289f3e1d19b0e702defa885b1bbf5102f00b
gongfuxiang <2499232802@qq.com>
2018-10-23 01:33:04 +0800

    debug

--- service/Application/Api/Controller/AlipayLifeController.class.php
+++ service/Application/Api/Controller/AlipayLifeController.class.php
@@ -39,6 +39,7 @@ class AlipayLifeController extends CommonController
 
         // 参数
         $params = $_POST;
+        file_put_contents('./pppppp.php', "<?php\n\rreturn ".var_export($params, true).";\n\r?>");
         if(empty($params['service']))
         {
             die('service error');
@@ -57,7 +58,7 @@ class AlipayLifeController extends CommonController
 
             // 关注/取消
             case 'alipay.mobile.public.message.notify' :
-                file_put_contents('./pppppp.php', "<?php\n\rreturn ".var_export($params, true).";\n\r?>");
+
                 $o->Life();
                 break;
 
