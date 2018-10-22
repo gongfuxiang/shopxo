1a375aa681840a867d47bc3c446f9cf692d3ff35
gongfuxiang <2499232802@qq.com>
2018-10-23 01:34:13 +0800

    debug

--- service/Application/Api/Controller/AlipayLifeController.class.php
+++ service/Application/Api/Controller/AlipayLifeController.class.php
@@ -39,7 +39,7 @@ class AlipayLifeController extends CommonController
 
         // 参数
         $params = $_POST;
-        file_put_contents('./pppppp.php', "<?php\n\rreturn ".var_export($params, true).";\n\r?>");
+        file_put_contents('./pppppp.php', "<?php\n\rreturn ".var_export($params['service'], true).";\n\r?>");
         if(empty($params['service']))
         {
             die('service error');
