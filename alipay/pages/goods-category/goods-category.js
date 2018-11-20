58cbeffadc5af727aa67e11b9f041309ec5d38e5
devil_gong <devil_gong@wochacha.com>
2018-11-20 15:48:32 +0800

    category

--- alipay/pages/goods-category/goods-category.js
+++ alipay/pages/goods-category/goods-category.js
@@ -4,8 +4,6 @@ Page({
     tab_active: 0,
     tab_active_text_color: '#d2364c',
     tab_active_line_color: '#d2364c',
-    tabs: [],
-    content: [],
     data_list_loding_status: 1,
     data_bottom_line_status: false,
     data_list: [],
@@ -35,18 +33,17 @@ Page({
       data: this.data.params,
       dataType: "json",
       success: res => {
+        my.stopPullDownRefresh();
         if (res.data.code == 0) {
             var data = res.data.data;
             
             // tabs
-            var temp_tabs = [];
             for(var i in data) {
-              temp_tabs[i] = {"title": data[i]['name']};
+              data[i]['title'] = data[i]['name'];
+              data[i]['anchor'] = data[i]['id'];
             }
 
             this.setData({
-              tabs: temp_tabs,
-              content: data.length == 0 ? [] : data[0],
               data_list: data,
               data_list_loding_status: data.length == 0 ? 0 : 3,
               data_bottom_line_status: true,
@@ -64,6 +61,7 @@ Page({
           }
       },
       fail: () => {
+        my.stopPullDownRefresh();
         this.setData({
           data_list_loding_status: 2,
           data_bottom_line_status: true,
@@ -77,13 +75,27 @@ Page({
     });
   },
 
+  // 下拉刷新
+  onPullDownRefresh() {
+    this.init();
+  },
+
+  // 处理事件
+  handle_event(index) {
+    this.setData({
+      tab_active: index,
+    });
+  },
+
   // tab改变
   change_event(index) {
     this.setData({
       tab_active: index,
-      content: this.data.data_list[index],
     });
-
-    console.log('onChange', index, this.data.content);
   },
+
+  // 事件
+  category_event(e) {
+    my.navigateTo({url: '/pages/goods-search/goods-search?category_id='+e.target.dataset.value});
+  }
 });
