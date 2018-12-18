3b2b449734e085cb9b0356e8f57f3007f3fca860
gongfuxiang <2499232802@qq.com>
2018-12-19 01:27:53 +0800

    slid

---
+++ application/service/SlideService.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\service;
+
+/**
+ * 轮播图服务层
+ * @author   Devil
+ * @blog     http://gong.gg/
+ * @version  0.0.1
+ * @datetime 2016-12-01T21:51:08+0800
+ */
+class SlideService
+{
+
+}
+?>
