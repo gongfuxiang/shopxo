7ad7a92b4e1e27feef82edc310dcfeb6471358b2
devil_gong <devil_gong@wochacha.com>
2018-12-25 13:20:57 +0800

    qrcode

--- application/index/controller/QrCode.php
+++ application/index/controller/QrCode.php
@@ -1,6 +1,5 @@
 <?php
-
-namespace Home\Controller;
+namespace app\index\controller;
 
 /**
  * 二维码生成控制层
@@ -9,7 +8,7 @@ namespace Home\Controller;
  * @version  0.0.1
  * @datetime 2016-12-01T21:51:08+0800
  */
-class QrCodeController extends CommonController
+class QrCode extends Common
 {
     /**
      * [__construct 构造方法]
@@ -24,13 +23,12 @@ class QrCodeController extends CommonController
      */
     public function Index()
     {
-        require_once APP_PATH.'Library'.DS.'Qrcode'.DS.'phpqrcode.php';
+        require_once ROOT.'extend'.DS.'qrcode'.DS.'phpqrcode.php';
         
         $level = isset($_REQUEST['level']) && in_array($_REQUEST['level'], array('L','M','Q','H')) ? $_REQUEST['level'] : 'L';
         $point_size = isset($_REQUEST['size']) ? min(max(intval($_REQUEST['size']), 1), 10) : 6;
         $mr = isset($_REQUEST['mr']) ? intval($_REQUEST['mr']) : 1;
         $content = isset($_REQUEST['content']) ? urldecode(trim($_REQUEST['content'])) : __MY_URL__;
-        //print_r($content);die;
         \QRcode::png($content, false, $level, $point_size, $mr);
     }
 }
