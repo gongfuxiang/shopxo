a65659b0c050b934e8c95c9e8a1a84d777c10264
devil_gong <devil_gong@wochacha.com>
2019-01-11 18:29:06 +0800

    debug

--- extend/phpmailer/PHPMailer.php
+++ extend/phpmailer/PHPMailer.php
@@ -3676,7 +3676,7 @@ class PHPMailer
                 //Include a link to troubleshooting docs on SMTP connection failure
                 //this is by far the biggest cause of support questions
                 //but it's usually not PHPMailer's fault.
-                return $this->language[$key] . ' https://github.com/PHPMailer/PHPMailer/wiki/Troubleshooting';
+                return $this->language[$key];
             }
 
             return $this->language[$key];
