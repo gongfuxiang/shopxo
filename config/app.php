c6cce399c5d8bcfb288a6b7dd970867d75468d37
gongfuxiang <2499232802@qq.com>
2019-02-21 22:56:26 +0800

    修复路由斜杠地址错误

--- config/app.php
+++ config/app.php
@@ -101,7 +101,7 @@ return [
     // 合并路由规则
     'route_rule_merge'       => false,
     // 路由是否完全匹配
-    'route_complete_match'   => false,
+    'route_complete_match'   => true,
     // 使用注解路由
     'route_annotation'       => false,
     // 域名根，如thinkphp.cn
