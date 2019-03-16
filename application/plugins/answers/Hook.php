f4c04d5a74210e5b88221a28f7db17940e888842
gongfuxiang <2499232802@qq.com>
2019-03-16 16:38:42 +0800

    游客购买应用

--- application/plugins/answers/Hook.php
+++ application/plugins/answers/Hook.php
@@ -22,46 +22,38 @@ use app\service\PluginsService;
  */
 class Hook extends Controller
 {
-    // 应用响应入口
+    /**
+     * 钩子入口
+     * @author   Guoguo
+     * @blog     http://gadmin.cojz8.com
+     * @version  1.0.0
+     * @datetime 2019年3月14日
+     */
     public function run($params = [])
     {
-        // 是否控制器钩子
-        // is_backend 当前为后端业务处理
-        // hook_name 钩子名称
-        if(isset($params['is_backend']) && $params['is_backend'] === true && !empty($params['hook_name']))
+        // 大导航前面添加问答地址
+        if(!empty($params['hook_name']) && $params['hook_name'] == 'plugins_service_navigation_header_handle')
         {
-            // 参数一   描述
-            // 参数二   0 为处理成功, 负数为失败
-            // 参数三   返回数据
-            return DataReturn('返回描述', 0);
-
-        // 默认返回视图
-        } else {
-            // 大导航前面添加问答地址
-            if(!empty($params['hook_name']) && $params['hook_name'] == 'plugins_service_navigation_header_handle')
+            if(is_array($params['header']))
             {
-                if(is_array($params['header']))
+                // 获取应用数据
+                $ret = PluginsService::PluginsData('answers', ['images']);
+                if($ret['code'] == 0 && !empty($ret['data']['application_name']))
                 {
-                    // 获取应用数据
-                    $ret = PluginsService::PluginsData('answers', ['images']);
-                    if($ret['code'] == 0 && !empty($ret['data']['application_name']))
-                    {
-                        $nav = [
-                            'id'                    => 0,
-                            'pid'                   => 0,
-                            'name'                  => $ret['data']['application_name'],
-                            'url'                   => PluginsHomeUrl('answers', 'index', 'index'),
-                            'data_type'             => 'custom',
-                            'is_show'               => 1,
-                            'is_new_window_open'    => 0,
-                            'items'                 => [],
-                        ];
-                        array_unshift($params['header'], $nav);
-                    }
+                    $nav = [
+                        'id'                    => 0,
+                        'pid'                   => 0,
+                        'name'                  => $ret['data']['application_name'],
+                        'url'                   => PluginsHomeUrl('answers', 'index', 'index'),
+                        'data_type'             => 'custom',
+                        'is_show'               => 1,
+                        'is_new_window_open'    => 0,
+                        'items'                 => [],
+                    ];
+                    array_unshift($params['header'], $nav);
                 }
             }
         }
-        return DataReturn('无需处理', 0);
     }
 }
 ?>
