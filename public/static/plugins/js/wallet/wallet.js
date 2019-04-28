283d28f23998d5e9680499b24bca97d419fe9cb3
gongfuxiang <2499232802@qq.com>
2019-04-29 01:07:52 +0800

    wallet

--- public/static/plugins/js/wallet/wallet.js
+++ public/static/plugins/js/wallet/wallet.js
@@ -6,13 +6,14 @@ $(function()
     // 混合列表选择
     $('.business-item ul li').on('click', function()
     {
+        var value = '';
         if($(this).hasClass('selected'))
         {
-            $('form.form-validation-plugins-recharge-pay input[name='+$(this).parent().data('type')+'_id]').val(0);
             $(this).removeClass('selected');
         } else {
-            $('form.form-validation-plugins-recharge-pay input[name='+$(this).parent().data('type')+'_id]').val($(this).data('value'));
+            value = $(this).data('value');
             $(this).addClass('selected').siblings('li').removeClass('selected');
         }
+        $('form.form-validation-plugins-recharge-pay input[name='+$(this).parent().data('type')+'_id]').val(value).blur();
     });
 });
