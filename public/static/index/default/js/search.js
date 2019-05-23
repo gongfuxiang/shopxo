7cac9388142b85d5fb8f988516aba7507111824a
gongfuxiang <2499232802@qq.com>
2019-05-24 01:07:53 +0800

    细节优化

--- public/static/index/default/js/search.js
+++ public/static/index/default/js/search.js
@@ -192,9 +192,9 @@ $(function()
                             html += '<img src="'+result.data.data[i]['images']+'" />';
                             html += '</a>';
                             html += '<a href="'+result.data.data[i]['goods_url']+'" target="_blank">';
-                            html += '<p class="title fl">'+result.data.data[i]['title']+'</p>';
+                            html += '<p class="am-text-truncate title fl">'+result.data.data[i]['title']+'</p>';
                             html += '</a>';
-                            html += '<p class="price fl"><b>¥</b><strong>'+result.data.data[i]['min_price']+'</strong></p>';
+                            html += '<p class="price fl"><strong>¥ '+result.data.data[i]['min_price']+'</strong></p>';
                             html += '<p class="number fl">销量<span>'+result.data.data[i]['sales_count']+'</span></p>';
                             html += '</div></li>';
 
