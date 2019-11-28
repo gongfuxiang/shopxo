bac5a807b6283821ca3e6f451008616048b13864
devil_gong <devil_gong@wochacha.com>
2019-11-28 11:17:21 +0800

    退款操作优化

--- application/service/OrderAftersaleService.php
+++ application/service/OrderAftersaleService.php
@@ -912,10 +912,12 @@ class OrderAftersaleService
         // 操作退款
         $pay_name = 'payment\\'.$pay_log['payment'];
         $pay_params = [
+            'order_id'          => $order['id'],
             'order_no'          => $order['order_no'],
             'trade_no'          => $pay_log['trade_no'],
             'pay_price'         => $order['pay_price'],
             'refund_price'      => $aftersale['price'],
+            'client_type'       => $order['client_type'],
             'refund_reason'     => $order['order_no'].'订单退款'.$aftersale['price'].'元',
         ];
         $ret = (new $pay_name($payment[0]['config']))->Refund($pay_params);
