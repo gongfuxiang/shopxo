1928fc7015d326d006fe97992c7445e8b335a52c
devil <fuxiang.gong@qq.com>
2019-12-26 17:34:04 +0800

    分类优化

--- public/static/index/default/js/category.js
+++ public/static/index/default/js/category.js
@@ -1,11 +1,22 @@
 $(function()
 {
-    // 分类显/隐
-    $('.category-list ul.category-nav li').on('mouseover', function(){
+    // 一级分类显/隐操作
+    $('.category-list ul.category-nav li').on('mouseover', function()
+    {
         var index = $(this).index();
         $('.category-list ul.category-nav li').removeClass('active');
         $(this).addClass('active');
         $('.category-content').addClass('none');
         $('.category-content-'+index).removeClass('none');
+    });
+
+    // 一级分类双击进入商品搜索页
+    $('.category-list ul.category-nav li').on('dblclick', function()
+    {
+        var url = $(this).data('url') || null;
+        if(url != null)
+        {
+            window.location.href = url;
+        }
     });
 });
