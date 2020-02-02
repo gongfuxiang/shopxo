f62a8682ff6429364451d35facb59ba910e1439a
devil <fuxiang.gong@qq.com>
2020-02-03 00:19:22 +0800

    小程序优化

--- public/appmini/old/alipay/app.js
+++ public/appmini/old/alipay/app.js
@@ -67,8 +67,8 @@ App({
 
     // 请求地址
     request_url: "{{request_url}}",
-     request_url: 'http://shopxo.com/',
-     request_url: 'http://dev.shopxo.net/',
+    // request_url: 'http://shopxo.com/',
+    // request_url: 'http://dev.shopxo.net/',
 
     // 基础信息
     application_title: "{{application_title}}",
