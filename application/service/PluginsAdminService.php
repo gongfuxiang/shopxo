61de2014484ac1e6ce69f3b6650a084e251d4ecb
Hk-tang <hktang@ualberta.ca>
2020-03-30 10:49:24 -0600

    Switch to direct access

--- application/service/PluginsAdminService.php
+++ application/service/PluginsAdminService.php
@@ -45,7 +45,7 @@ class PluginsAdminService
             {
                 while(($temp_file = readdir($dh)) !== false)
                 {
-                    if(!in_array($temp_file, ['.', '..', '/', 'view', 'index.html']) && substr($temp_file, 0, 1) != '.')
+                    if(!in_array($temp_file, ['.', '..', '/', 'view', 'index.html']) && $temp_file[0] != '.')
                     {
                         // 获取插件配置信息
                         $config = self::GetPluginsConfig($temp_file);
