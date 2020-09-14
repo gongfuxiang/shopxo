c61c7aae5e596dda2a71d2294a19382e0ffd1002
devil <fuxiang.gong@qq.com>
2020-09-14 23:21:42 +0800

    小程序快捷导航优化

--- sourcecode/weixin/components/quick-nav/quick-nav.js
+++ sourcecode/weixin/components/quick-nav/quick-nav.js
@@ -2,13 +2,39 @@ const app = getApp();
 Component({
   data: {
     popup_status: false,
-    data: [],
+    data_list: [],
+    is_first: 1,
+  },
+  pageLifetimes: {
+    // 页面被展示
+    show: function() {
+      this.init_config();
+
+      // 非首次进入则校验数据，无数据则重新初始化配置接口
+      if(this.data.is_first == 0) {
+        var data = app.get_config('quick_nav') || [];
+        if(data.length <= 0) {
+          app.init_config();
+        }
+      }
+
+      // 首次初始化状态
+      this.setData({ is_first: 0 });
+    },
   },
   methods: {
+    // 初始化配置
+    init_config(status) {
+      if((status || false) == true) {
+        this.setData({ data_list: app.get_config('quick_nav') || [] });
+      } else {
+        app.is_config(this, 'init_config');
+      }
+    },
+
     // 弹层开启
     quick_open_event(e) {
-      var data = app.get_config('quick_nav') || [];
-      this.setData({ popup_status: true, data: data });
+      this.setData({popup_status: true, data_list: app.get_config('quick_nav') || []});
     },
 
     // 弹层关闭
