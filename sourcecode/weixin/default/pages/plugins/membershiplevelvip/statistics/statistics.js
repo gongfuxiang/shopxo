8f344c44e404059ed6d657feae418df13a2b8dc5
Devil <fuxiang.gong@qq.com>
2020-12-09 17:49:35 +0800

    减小微信小程序包

--- sourcecode/weixin/default/pages/plugins/membershiplevelvip/statistics/statistics.js
+++ sourcecode/weixin/default/pages/plugins/membershiplevelvip/statistics/statistics.js
@@ -1,28 +1,4 @@
-import * as echarts from '../../../../components/ec-canvas/echarts';
 const app = getApp();
-
-// 近15日收益走势
-let profit_chart_obj = null;
-function init_profit_chart(canvas, width, height) {
-  profit_chart_obj = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(profit_chart_obj);
-  return profit_chart_obj;
-};
-
-// 近15日推广用户数
-let user_chart_obj = null;
-function init_user_chart(canvas, width, height) {
-  user_chart_obj = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(user_chart_obj);
-  return user_chart_obj;
-};
-
 Page({
   data: {
     data_list_loding_status: 1,
@@ -35,14 +11,6 @@ Page({
     user_profit_total_price: 0.00,
     user_data: null,
     profit_data: null,
-
-    // 图表
-    profit_chart: {
-      onInit: init_profit_chart,
-    },
-    user_chart: {
-      onInit: init_user_chart,
-    },
 
     // 基础配置
     currency_symbol: app.data.currency_symbol,
@@ -97,16 +65,6 @@ Page({
             data_bottom_line_status: true,
             data_list_loding_msg: '',
           });
-
-          // 图表
-          setTimeout(function()
-          {
-            // 近15日收益走势
-            self.set_profit_chart(data.profit_chart);
-
-            // 近15日推广用户数
-            self.set_profit_user(data.user_chart);
-          }, 200);
         } else {
           self.setData({
             data_list_loding_status: 2,
@@ -130,86 +88,6 @@ Page({
         app.showToast("服务器请求出错");
       }
     });
-  },
-
-  // 近15日推广用户数
-  set_profit_user(data) {
-    if ((data || null) != null) {
-      var option = {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              backgroundColor: '#6a7985'
-            }
-          }
-        },
-        grid: {
-          top: '10%',
-          left: '5%',
-          right: '5%',
-          bottom: '15%',
-          containLabel: true
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: data.name_arr
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: data.data,
-          type: 'bar',
-          areaStyle: {}
-        }]
-      };
-      if (typeof (user_chart_obj) == 'object') {
-        user_chart_obj.setOption(option);
-      }
-    }
-  },
-
-  // 近15日收益走势图表
-  set_profit_chart(data) {
-    if ((data || null) != null)
-    {
-      var option = {
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              backgroundColor: '#6a7985'
-            }
-          }
-        },
-        grid: {
-          top: '10%',
-          left: '5%',
-          right: '5%',
-          bottom: '15%',
-          containLabel: true
-        },
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: data.name_arr
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [{
-          data: data.data,
-          type: 'line'
-        }]
-      };
-      if (typeof (profit_chart_obj) == 'object') {
-        profit_chart_obj.setOption(option);
-      }
-    }
   },
 
   // 下拉刷新
