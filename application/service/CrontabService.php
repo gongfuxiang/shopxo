601611153e9e82db7c136b151104ce58f21e0710
Devil <fuxiang.gong@qq.com>
2021-07-12 15:24:00 +0800

    订单自动收货参数修复

--- application/service/CrontabService.php
+++ application/service/CrontabService.php
@@ -126,7 +126,7 @@ class CrontabService
                     if($ret['code'] == 0)
                     {
                         // 订单商品销量增加
-                        $ret = OrderService::GoodsSalesCountInc(['order_id'=>$v['id']]);
+                        $ret = OrderService::GoodsSalesCountInc(['order_id'=>$v['id'], 'order_status'=>$upd_data['status']]);
                         if($ret['code'] == 0)
                         {
                             // 用户消息
