00fbf91ecaea486fbdab059f3cb9bf5e82e26d14
Devil <fuxiang.gong@qq.com>
2021-07-24 00:12:12 +0800

    数据库配置关闭sql监听

--- app/install/controller/Index.php
+++ app/install/controller/Index.php
@@ -301,9 +301,9 @@ return [
             // 是否需要断线重连
             'break_reconnect' => false,
             // 监听SQL
-            'trigger_sql'     => true,
+            'trigger_sql'     => false,
             // 开启字段缓存
-            'fields_cache'    => false,
+            'fields_cache'    => true,
         ]
     ]
 ];
