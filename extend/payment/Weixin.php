39a9434e828a3b23faa26f1006c959fc92a6889f
Devil <fuxiang.gong@qq.com>
2021-07-26 03:04:23 +0800

    微信支付优化

--- extend/payment/Weixin.php
+++ extend/payment/Weixin.php
@@ -50,7 +50,7 @@ class Weixin
         // 基础信息
         $base = [
             'name'          => '微信',  // 插件名称
-            'version'       => '1.1.1',  // 插件版本
+            'version'       => '1.1.2',  // 插件版本
             'apply_version' => '不限',  // 适用系统版本描述
             'apply_terminal'=> ['pc', 'h5', 'ios', 'android', 'weixin', 'qq'], // 适用终端 默认全部 ['pc', 'h5', 'app', 'alipay', 'weixin', 'baidu']
             'desc'          => '适用公众号+PC+H5+APP+微信小程序，即时到帐支付方式，买家的交易资金直接打入卖家账户，快速回笼交易资金。 <a href="https://pay.weixin.qq.com/" target="_blank">立即申请</a>',  // 插件描述（支持html）
@@ -759,8 +759,8 @@ class Weixin
     private function GetApiclientFile()
     {
         // 证书位置
-        $apiclient_cert_file = ROOT.'runtime'.DS.'temp'.DS.'payment_weixin_pay_apiclient_cert.pem';
-        $apiclient_key_file = ROOT.'runtime'.DS.'temp'.DS.'payment_weixin_pay_apiclient_key.pem';
+        $apiclient_cert_file = ROOT.'runtime'.DS.'data'.DS.'payment_weixin_pay_apiclient_cert.pem';
+        $apiclient_key_file = ROOT.'runtime'.DS.'data'.DS.'payment_weixin_pay_apiclient_key.pem';
 
         // 证书处理
         if(stripos($this->config['apiclient_cert'], '-----') === false)
