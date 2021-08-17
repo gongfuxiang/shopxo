65da8bb3bf3426e9578ca17123728a8bd89815c7
Devil <fuxiang.gong@qq.com>
2021-08-17 11:29:52 +0800

    微信支付 urlencode 处理

--- extend/payment/Weixin.php
+++ extend/payment/Weixin.php
@@ -50,7 +50,7 @@ class Weixin
         // 基础信息
         $base = [
             'name'          => '微信',  // 插件名称
-            'version'       => '1.1.2',  // 插件版本
+            'version'       => '1.1.3',  // 插件版本
             'apply_version' => '不限',  // 适用系统版本描述
             'apply_terminal'=> ['pc', 'h5', 'ios', 'android', 'weixin', 'qq'], // 适用终端 默认全部 ['pc', 'h5', 'app', 'alipay', 'weixin', 'baidu']
             'desc'          => '适用公众号+PC+H5+APP+微信小程序，即时到帐支付方式，买家的交易资金直接打入卖家账户，快速回笼交易资金。 <a href="https://pay.weixin.qq.com/" target="_blank">立即申请</a>',  // 插件描述（支持html）
@@ -257,7 +257,7 @@ class Weixin
             case 'MWEB' :
                 if(!empty($params['order_id']))
                 {
-                    $data['mweb_url'] .= '&redirect_url='.$redirect_url;
+                    $data['mweb_url'] .= '&redirect_url='.urlencode($redirect_url);
                 }
                 $result = DataReturn('success', 0, $data['mweb_url']);
                 break;
