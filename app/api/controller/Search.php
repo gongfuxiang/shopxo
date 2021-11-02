91cc4a491553df0db5e6d7636ebd868feea97818
gongfuxiang <fuxiang.gong@qq.com>
2021-11-03 00:05:51 +0800

    搜索接口初始化拆分

--- app/api/controller/Search.php
+++ app/api/controller/Search.php
@@ -37,7 +37,7 @@ class Search extends Common
     }
 
     /**
-     * 商品搜索
+     * 搜索初始化
      * @author   Devil
      * @blog    http://gong.gg/
      * @version 1.0.0
@@ -46,33 +46,40 @@ class Search extends Common
      */
     public function Index()
     {
+        $result = [
+            // 指定数据
+            'search_map_info'       => SearchService::SearchMapInfo($this->data_request),
+            // 品牌列表
+            'brand_list'            => SearchService::CategoryBrandList($this->data_request),
+            // 商品分类
+            'category_list'         => SearchService::GoodsCategoryList($this->data_request),
+            // 筛选价格区间
+            'screening_price_list'  => SearchService::ScreeningPriceList($this->data_request),
+            // 商品参数
+            'goods_params_list'     => SearchService::SearchGoodsParamsValueList($this->data_request),
+            // 商品规格
+            'goods_spec_list'       => SearchService::SearchGoodsSpecValueList($this->data_request),
+        ];
+        return ApiService::ApiDataReturn(SystemBaseService::DataReturn($result));
+    }
+
+    /**
+     * 数据列表
+     * @author   Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2018-07-12
+     * @desc    description
+     */
+    public function DataList()
+    {
         // 搜索记录
         $this->data_post['user_id'] = isset($this->user['id']) ? $this->user['id'] : 0;
         SearchService::SearchAdd($this->data_post);
 
         // 获取数据
         $ret = SearchService::GoodsList($this->data_post);
-        $result = $ret['data'];
-
-        // 品牌列表
-        $result['brand_list'] = SearchService::CategoryBrandList($this->data_request);
-
-        // 指定数据
-        $result['search_map_info'] = SearchService::SearchMapInfo($this->data_request);
-
-        // 商品分类
-        $result['category_list'] = SearchService::GoodsCategoryList($this->data_request);
-
-        // 筛选价格区间
-        $result['screening_price_list'] = SearchService::ScreeningPriceList($this->data_request);
-
-        // 商品参数
-        $result['goods_params_list'] = SearchService::SearchGoodsParamsValueList($this->data_request);
-
-        // 商品规格
-        $result['goods_spec_list'] = SearchService::SearchGoodsSpecValueList($this->data_request);
-
-        return ApiService::ApiDataReturn(SystemBaseService::DataReturn($result));
+        return ApiService::ApiDataReturn(SystemBaseService::DataReturn($ret['data']));
     }
 }
 ?>
