8e7f483c47ca77e3ce10706cb79601d969abc266
gongfuxiang <fuxiang.gong@qq.com>
2022-03-04 17:28:10 +0800

    新增挂账支付方式

---
+++ extend/payment/ChargePayment.php
@@ -0,0 +1,102 @@
+<?php
+// +----------------------------------------------------------------------
+// | ShopXO 国内领先企业级B2C免费开源电商系统
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011~2099 http://shopxo.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( https://opensource.org/licenses/mit-license.php )
+// +----------------------------------------------------------------------
+// | Author: Devil
+// +----------------------------------------------------------------------
+namespace payment;
+
+/**
+ * 挂账支付
+ * @author   Devil
+ * @blog    http://gong.gg/
+ * @version 1.0.0
+ * @date    2018-09-19
+ * @desc    description
+ */
+class ChargePayment
+{
+    // 插件配置参数
+    private $config;
+
+    /**
+     * 构造方法
+     * @author   Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2018-09-17
+     * @desc    description
+     * @param   [array]           $params [输入参数（支付配置参数）]
+     */
+    public function __construct($params = [])
+    {
+        $this->config = $params;
+    }
+
+    /**
+     * 配置信息
+     * @author   Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2018-09-19
+     * @desc    description
+     */
+    public function Config()
+    {
+        // 基础信息
+        $base = [
+            'name'          => '挂账支付',  // 插件名称
+            'version'       => '1.0.0',  // 插件版本
+            'apply_version' => '不限',  // 适用系统版本描述
+            'desc'          => '用于线下收款，挂账、赊账、月结类型客户使用',  // 插件描述（支持html）
+            'author'        => 'Devil',  // 开发者
+            'author_url'    => 'http://shopxo.net/',  // 开发者主页
+        ];
+
+        // 配置信息
+        $element = [
+        ];
+
+        return [
+            'base'      => $base,
+            'element'   => $element,
+        ];
+    }
+
+    /**
+     * 支付入口
+     * @author   Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2018-09-19
+     * @desc    description
+     * @param   [array]           $params [输入参数]
+     */
+    public function Pay($params = [])
+    {
+        $url = $params['call_back_url'].'?';
+        $url .= 'out_trade_no='.$params['order_no'];
+        $url .= '&subject='.$params['name'];
+        $url .= '&total_price='.$params['total_price'];
+        return DataReturn('处理成功', 0, $url);
+    }
+
+    /**
+     * 支付回调处理
+     * @author   Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2018-09-19
+     * @desc    description
+     * @param   [array]           $params [输入参数]
+     */
+    public function Respond($params = [])
+    {
+        return DataReturn('处理成功', 0, $params);
+    }
+}
+?>
