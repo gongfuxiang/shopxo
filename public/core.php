eb475e9e15bae0d39a482d706b9d77762f4d0d2f
gongfuxiang <fuxiang.gong@qq.com>
2022-03-08 12:02:12 +0800

    v2.2.5

--- public/core.php
+++ public/core.php
@@ -10,7 +10,7 @@
 // +----------------------------------------------------------------------
 
 // 系统版本
-define('APPLICATION_VERSION', 'v2.2.4');
+define('APPLICATION_VERSION', 'v2.2.5');
 
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'7.4.0','<'))
